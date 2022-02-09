726a458e5de5b681ef546d2fd02c78216953f22c
Yorguin José Mantilla Ramos <36543115+yjmantilla@users.noreply.github.com>
2022-02-08 22:47:14 -0500

    Lo hecho en 2022-02-05 NeuroCo

--- 6 - play/sketch.js
+++ 6 - play/sketch.js
@@ -2,9 +2,18 @@ function setup() {
   // put setup code here
     createCanvas(640,480);
 }
-
+i=0;
 function draw() {
     background(100);
-    circle(320, 240, 340);
+    avance=64;
+    // for(i=0;i<10;i++){
+        // console.log(i);
+        // circle(i*avance, 240, 34);
+    // }
+    circle(i*avance, 240, 34);
     fill(0,0,0)
+    i=i+avance;
+    // if (i > 640){
+    // i=i%640;
+    // }
 }
--- x - beat/sketch.js
+++ x - beat/sketch.js
@@ -3,12 +3,15 @@ function setup() {
     createCanvas(700,700);
     frameRate(10);
 }
-
 states = [100,100,100,100,100,200,300];
 i=0;
+
 function draw() {
     background(100);
-    heart(350, 300, states[i%states.length]);
+    const rndInt = randomIntFromInterval(100, 500)
+    console.log(rndInt)
+    
+    heart(350, 300, rndInt);
     fill(255,0,0);
     i = i+1;
 }
@@ -21,3 +24,8 @@ function heart(x, y, size) {
   bezierVertex(x + size, y + size / 3, x + size / 2, y - size / 2, x, y);
   endShape(CLOSE);
 }
+
+function randomIntFromInterval(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
